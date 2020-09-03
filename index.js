cbc8e70871beb2d35471b90b454c42b0c32e9b22
matheus <mtava002@gmail.com>
2020-09-03 17:21:52 -0400

    heyoooooo

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var people = {};
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-});
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-    });
-});
+
+// io.on('connection', (socket) => {
+//     socket.on('connection', function (client) {
+//         client.on('join', function (name) {
+//             people[client.id] = name;
+//             client.emit("update", "You have connected to the server.");
+//             s
+
+//         })
+//     })
+//     console.log('a user connected');
+//     socket.on('disconnect', () => {
+//         console.log('user disconnected');
+//     });
+// });
+
+// io.on('connection', (socket) => {
+//     socket.on('chat message', (msg) => {
+//         console.log('message: ' + msg);
+//     });
+// });
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
